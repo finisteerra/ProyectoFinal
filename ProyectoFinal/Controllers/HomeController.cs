be240ce44fd13bf246c070aa80bd76b287f2ebfc
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-17 23:04:41 -0600

    Registrar Bitacora y RecuperarContraseña

--- ProyectoFinal/Controllers/HomeController.cs
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -10,7 +10,8 @@ namespace ProyectoFinal.Controllers
 {
     public class HomeController : Controller
     {
-        UsuariosModel usuariosModel = new UsuariosModel();
+         UsuariosModel usuariosModel = new UsuariosModel();
+         LogsModel logsModel = new LogsModel();
 
         //Métodos de Iniciar Sesión
 
@@ -32,6 +33,73 @@ namespace ProyectoFinal.Controllers
                     ViewBag.mensaje = "Sus credenciales no fueron validados";
                     return View("Index");
                 }
+        }
+        
+        //Métodos de Iniciar Sesión
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IniciarSesion(UsuariosEnt entidad)
+        {
+            var resultado = usuariosModel.ValidarUsuario(entidad);
+
+            if (resultado)
+                return View();
+            else
+            {
+                ViewBag.mensaje = "Sus credenciales no fueron validados";
+                return View("Index");
+            }
+        }
+
+        //Recuperar Contraseña
+
+        [HttpPost]
+        public ActionResult BuscarCorreo(string ValidarCorreo)
+        {
+            return Json(usuariosModel.BuscarCorreo(ValidarCorreo), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult RecuperarContrasenna()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                RegistrarBitacora(ex, ControllerContext);
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarContrasenna(UsuariosEnt entidad)
+        {
+            try
+            {
+                usuariosModel.RecuperarContrasenna(entidad);
+                return View("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+        }
+
+
+        public void RegistrarBitacora(Exception ex, ControllerContext ubicacion)
+        {
+            LogsEnt logs = new LogsEnt();
+            logs.origen = ubicacion.RouteData.Values["controller"].ToString() + "-" + ubicacion.RouteData.Values["action"].ToString();
+            logs.mensajeError = ex.Message;
+            logsModel.RegistrarBitacora(logs);
         }
         
     }
