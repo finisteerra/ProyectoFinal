389d5013363d2b4701c966004f61e4dd6595b45b
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-17 23:06:50 -0600

    BuscarCorreo y RecuperarContraseña

--- ProyectoFinal/Models/UsuariosModel.cs
+++ ProyectoFinal/Models/UsuariosModel.cs
@@ -25,6 +25,31 @@ namespace ProyectoFinal.Models
                 return true;
             }
         }
+        
+        public string BuscarCorreo(string ValidarCorreo)
+        {
+            using (var client = new HttpClient())
+            {
+
+                string url = "https://localhost:44372/api/BuscarCorreo?ValidarCorreo=" + ValidarCorreo;
+                HttpResponseMessage respuesta = client.GetAsync(url).GetAwaiter().GetResult();
+                if (respuesta.IsSuccessStatusCode)
+                    return respuesta.Content.ReadFromJsonAsync<string>().Result;
+
+            }
+            return "ERROR";
+        }
+
+
+        public void RecuperarContrasenna(UsuariosEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                JsonContent body = JsonContent.Create(entidad);
+                string url = "https://localhost:44372/api/RecuperarContrasenna";
+                HttpResponseMessage respuesta = client.PostAsync(url, body).GetAwaiter().GetResult();
+            }
+        }
 
     }
 }
