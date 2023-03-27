fbd033de6fc05e22f03901eba1f819dbb5ee4b8e
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-27 17:10:03 -0600

    Add files via upload

---
+++ ProyectoFinal/Entities/RolesEnt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal.Entities
+{
+    public class RolesEnt
+    {
+        public long ConsecutivoRol { get; set; }
+
+        public string TipoRol { get; set; }
+
+        
+    }
+}
