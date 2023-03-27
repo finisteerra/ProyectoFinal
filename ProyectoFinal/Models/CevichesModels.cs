e6bf809ca85661690539a83844053f20e75159fb
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-27 13:47:45 -0600

    Add files via upload

---
+++ ProyectoFinal/Models/CevichesModels.cs
@@ -0,0 +1,29 @@
+﻿using ProyectoFinal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web;
+
+namespace ProyectoFinal.Models
+{
+    public class CevichesModels
+    {
+        public List<CevichesEnt> ConsultarCeviches()
+        {
+            using (var client = new HttpClient())
+            {
+                string url = "https://localhost:44372/api/ConsultarCeviches";
+
+                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Current.Session["Token"].ToString());
+                HttpResponseMessage respuesta = client.GetAsync(url).GetAwaiter().GetResult();
+
+                if (respuesta.IsSuccessStatusCode)
+                    return respuesta.Content.ReadFromJsonAsync<List<CevichesEnt>>().Result;
+
+                return new List<CevichesEnt>();
+            }
+        }
+    }
+}
---
+++ ProyectoFinal/Models/ProveedoresModels.cs
@@ -0,0 +1,29 @@
+﻿using ProyectoFinal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web;
+
+namespace ProyectoFinal.Models
+{
+    public class ProveedoresModels
+    {
+        public List<ProveedoresEnt> ConsultarProveedores()
+        {
+            using (var client = new HttpClient())
+            {
+                string url = "https://localhost:44372/api/ConsultarProveedores";
+
+                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Current.Session["Token"].ToString());
+                HttpResponseMessage respuesta = client.GetAsync(url).GetAwaiter().GetResult();
+
+                if (respuesta.IsSuccessStatusCode)
+                    return respuesta.Content.ReadFromJsonAsync<List<ProveedoresEnt>>().Result;
+
+                return new List<ProveedoresEnt>();
+            }
+        }
+    }
+}
